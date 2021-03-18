74ddc16da4a08bbc18bdd63609364a7d52a14079
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-19 00:51:52 +0100

    Solution for the example 10

---
+++ ex10/src/index.js
@@ -0,0 +1,12 @@
+function mySplice(arr1, arr2, n) {
+    for(var i = 0; i < arr1.length; i++) {
+        arr2.splice(n, 0, arr1[i]);
+        }
+        return arr2;
+}
+
+console.log(mySplice([1, 2, 3], [4, 5], 1));
+console.log(mySplice([1, 2, 3], [4, 5], 2));
+console.log(mySplice(["b", "c"], ["a", "d"], 1));
+console.log(mySplice(["Dell", "Phillips"], ["Samsung", "AOX", "Sony"], 2));
+module.exports = mySplice;
